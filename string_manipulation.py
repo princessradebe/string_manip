521e00f3ad0b79ed18c0e2cf4b055f6f454c1069
princessradebe <princessradebe9819@gmail.com>
2024-09-10 04:59:38 +0200

    fix errors

--- string_manipulation.py
+++ string_manipulation.py
@@ -2,10 +2,10 @@
 
 sentence = "money!is!the!root!of!evil"
 reverse_sentence = sentence[:: -1]
-upper_sentence = (sentence.upper()) 
+upper_sentence = (sentence.upper())
 new_sentence = sentence.replace("!", " ")
 
-print (sentence)
-print (new_sentence)
-print (reverse_sentence)
-print (upper_sentence)
+print(sentence)
+print(new_sentence)
+print(reverse_sentence)
+print(upper_sentence)
