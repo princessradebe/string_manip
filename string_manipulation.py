e38211c20c06a885fe342ab8bc52abf1bcd0a0d0
princessradebe <princessradebe9819@gmail.com>
2024-09-10 05:07:06 +0200

    add cmments

--- string_manipulation.py
+++ string_manipulation.py
@@ -1,11 +1,19 @@
 # String manipulation
 
 sentence = "money!is!the!root!of!evil"
+# This code writes the string in reverse
 reverse_sentence = sentence[:: -1]
+# This code writes the string uppercase letters
 upper_sentence = (sentence.upper()) 
+# This codes replaces the exclamation mark with space in the program
 new_sentence = sentence.replace("!", " ")
 
+# This code prints all the sentences
+print('String')
 print (sentence)
+print('String without exclamation marks')
 print (new_sentence)
-print (reverse_sentence)
+print('String in uppercase letters')
 print (upper_sentence)
+print('String reversed')
+print (reverse_sentence)
