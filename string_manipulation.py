9c2c57b61d59f5c0b729c8d184d43f28ae55f083
princessradebe <princessradebe9819@gmail.com>
2024-09-10 04:53:44 +0200

    Create string_manipulation.py

---
+++ string_manipulation.py
@@ -0,0 +1,11 @@
+# String manipulation
+
+sentence = "money!is!the!root!of!evil"
+reverse_sentence = sentence[:: -1]
+upper_sentence = (sentence.upper()) 
+new_sentence = sentence.replace("!", " ")
+
+print (sentence)
+print (new_sentence)
+print (reverse_sentence)
+print (upper_sentence)
